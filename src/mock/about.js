8843123c45c7f96c09b10c1a3b31183e71e36861
tianhao <1664927010@qq.com>
2022-06-07 14:11:48 +0800

    update data

--- src/mock/about.js
+++ src/mock/about.js
@@ -4,8 +4,8 @@ Mock.mock("/api/about", "get", {
   msg: "",
   // data: "http://tianhao-imglist.dalivan.tech/",
   // data: "https://github.com/wjgsh",
-  // data:
-  //   "https://rp.mockplus.cn/run/JgWDcs70EIrM/t0cbK5Z0t?cps=expand&rps=collapse&nav=1&ha=0&la=0&fc=0&dt=iphoneX&out=0&rt=1",
+  data:
+    "https://rp.mockplus.cn/run/JgWDcs70EIrM/t0cbK5Z0t?cps=expand&rps=collapse&nav=1&ha=0&la=0&fc=0&dt=iphoneX&out=0&rt=1",
   // data: "https://baidu.com",
-  data: "https://wjgsh.github.io/talk/",
+  // data: "https://wjgsh.github.io/talk/",
 });
